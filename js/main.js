4d22c0ac2dea47d92600cb10499e00be2a1effc2
Tu Trinh <tutrinh@gmail.com>
2016-11-08 11:34:12 -0600

    Tu: Init

--- js/main.js
+++ js/main.js
@@ -5,5 +5,56 @@ jQuery( document ).ready(function( $ ) {
         $('.fullHeight').setFullHeight();
     });
 
+    //Chosen select
+    jQuery("select.chosen-select").chosen({width:'100%'});
+
+    if($.fn.chosen){
+        if($('.chosen-select').length){
+            jQuery('.chosen-select').on('change', function (evt, params) {
+                var selectedValue = params.selected;
+                var arr = [];
+                console.log(this.value);
+                arr.push(selectedValue);
+                arr.push( $( ".chosen-select option:selected" ).text());
+                $('#chosen-output').text(arr.join(", "));
+                console.log($( ".chosen-select option:selected" ).text());//output the text value
+            });
+        }
+    }
+
+    //Datatables
+    if($.fn.dataTable){
+        $('.dataTable').dataTable();
+        $('.dataTables_length select.form-control').chosen();
+    }
+
+    //Datepicker call
+    if($.fn.datepicker){
+        if($('.datepicker').length){
+            $('.datepicker').datepicker({
+                autoclose: true
+                //,endDate: '-1d'
+            });
+        }
+    }
+
+    //Radio button value
+    $('.radio').on('toggle',function(e) {
+        var $el = $(e.target);
+        var elname = e.target.name;
+        console.log($el.val());
+        console.log(elname);
+    });
+
+    //Checkbox button value
+    $('.checkbox').on('toggle',function(e) {
+        var $el = $(e.target);
+        var elname = e.target.name;
+        console.log($el.val());
+        console.log(elname);
+        console.log($el.prop("checked"));
+    });
+
+
 
 });
